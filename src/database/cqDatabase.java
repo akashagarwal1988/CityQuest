351aa1f13cbc1f6bf1076b3bf11271fdc214e9c6
akasha <akasha@yahoo-inc.com>
2014-04-20 22:17:52 -0700

    add

---
+++ src/database/cqDatabase.java
@@ -0,0 +1,35 @@
+package database;
+
+import java.net.UnknownHostException;
+import java.util.Set;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class cqDatabase {
+	
+	public static void main(String args[]) throws UnknownHostException{
+		makeConnection();
+	}
+	
+	public static void makeConnection() throws UnknownHostException{
+		
+		 MongoClientURI uri  = new MongoClientURI("mongodb://cq_api:cqdemo@ds053778.mongolab.com:53778/cityquest"); 
+	        MongoClient client = new MongoClient(uri);
+	        DB db = client.getDB(uri.getDatabase());
+	        Set<String> names = db.getCollectionNames();
+	        System.out.println(names);
+	        DBCollection collection = db.getCollection("testCollection");
+	        DBCursor cursor = collection.find();
+	    	while(cursor.hasNext()) {
+	    	    System.out.println(cursor.next());
+	    	}
+	        
+		
+	}
+
+}

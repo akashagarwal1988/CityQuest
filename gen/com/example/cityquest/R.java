51c053f621b5309a2651341d1e22302c6b58e254
earthbender06 <sisodia.prithvi@gmail.com>
2014-04-23 09:30:59 +0530

    commit

--- gen/com/example/cityquest/R.java
+++ gen/com/example/cityquest/R.java
@@ -34,12 +34,12 @@ public final class R {
         public static final int share_button=0x7f090003;
         public static final int share_category_name=0x7f090002;
         public static final int share_contact_details=0x7f090006;
-        public static final int share_location_button=0x7f09000b;
-        public static final int share_photo_button=0x7f09000a;
+        public static final int share_location_button=0x7f09000a;
+        public static final int share_photo_button=0x7f090009;
         public static final int share_place_name=0x7f090005;
-        public static final int share_ratings_bar=0x7f090009;
+        public static final int share_ratings_bar=0x7f09000c;
         public static final int share_review=0x7f090007;
-        public static final int share_tags_button=0x7f09000c;
+        public static final int share_tags_button=0x7f09000b;
         public static final int share_top_label=0x7f090001;
     }
     public static final class layout {
